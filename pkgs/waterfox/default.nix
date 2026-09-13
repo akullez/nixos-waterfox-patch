1726dd157b942cdeb8146ba316ff25cb3df26df4
Alexey Kotovski <alexeykotovski80@gmail.com>
2026-09-13 22:06:07 +0400

    Update default.nix

--- pkgs/waterfox/default.nix
+++ pkgs/waterfox/default.nix
@@ -1,34 +1,34 @@
-{ stdenv, lib, fetchurl, autoPatchelfHook, makeWrapper,
-  xorg, gtk3, alsa-lib, dbus, glib, pango, nss, nspr, atk, pciutils, 
-  mesa, systemd, libnotify, fontconfig, freetype,
-  libGL, libuuid, libxkbcommon, libdrm, wayland, cairo, gdk-pixbuf, ffmpeg
-}:
+{ lib, stdenv, fetchurl, buildFHSUserEnv }:
 
-stdenv.mkDerivation rec {
-  pname = "waterfox";
+let
   version = "6.7.3";
+  
+  waterfox-extracted = stdenv.mkDerivation {
+    pname = "waterfox-extracted";
+    inherit version;
+    
+    src = fetchurl {
+      url = "https://cdn1.waterfox.net/waterfox/releases/${version}/Linux_x86_64/waterfox-${version}.tar.bz2";
+      hash = "sha256-2Pd9NDlHZDWLSsNlRzOJtGuArvWRVr+eD3K59hys0i8=";
+    };
 
-  src = fetchurl {
-    url = "https://cdn1.waterfox.net/waterfox/releases/${version}/Linux_x86_64/waterfox-${version}.tar.bz2";
-    hash = "sha256-2Pd9NDlHZDWLSsNlRzOJtGuArvWRVr+eD3K59hys0i8=";
+    installPhase = ''
+      mkdir -p $out/opt/waterfox
+      cp -r * $out/opt/waterfox/
+    '';
   };
+in
 
-  nativeBuildInputs = [ autoPatchelfHook makeWrapper ];
-
-  buildInputs = [
-    stdenv.cc.cc.lib 
-    gtk3 alsa-lib dbus glib pango nss nspr atk pciutils libuuid
-    mesa systemd libnotify fontconfig freetype libGL
-    libxkbcommon libdrm wayland cairo gdk-pixbuf ffmpeg
+buildFHSUserEnv {
+  name = "waterfox";
+  
+  targetPkgs = pkgs: with pkgs; [
+    alsa-lib dbus glib pango nss nspr atk pciutils
+    mesa systemd libnotify fontconfig freetype
+    libGL libuuid libxkbcommon libdrm wayland cairo gdk-pixbuf ffmpeg
     xorg.libX11 xorg.libxcb xorg.libXcomposite xorg.libXdamage
     xorg.libXext xorg.libXfixes xorg.libXrandr xorg.libXrender xorg.libXtst
   ];
 
-  installPhase = ''
-    mkdir -p $out/bin $out/opt/waterfox
-    cp -r * $out/opt/waterfox/
-    
-    makeWrapper $out/opt/waterfox/waterfox $out/bin/waterfox \
-      --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath buildInputs}"
-  '';
+  runScript = "${waterfox-extracted}/opt/waterfox/waterfox";
 }
