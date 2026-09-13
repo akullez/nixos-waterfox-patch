06a29413b0b5a77461d9fa0dc7259b19fd41672c
Alexey Kotovski <alexeykotovski80@gmail.com>
2026-09-13 20:08:38 +0400

    Update default.nix

--- pkgs/waterfox/default.nix
+++ pkgs/waterfox/default.nix
@@ -10,26 +10,27 @@
 let
 in
 
-(buildMozillaMach rec {
-  pname = "waterfox";
-  applicationName = "Waterfox";
-  binaryName = "waterfox";
-  version = "6.5.6";
+(
+  (buildMozillaMach rec {
+    pname = "waterfox";
+    applicationName = "Waterfox";
+    binaryName = "waterfox";
+    version = "6.5.6";
 
-  src = fetchFromGitHub {
-    owner = "BrowserWorks";
-    repo = "waterfox";
-    rev = "refs/tags/${version}";
-    hash = "sha256-MTlA/6R7opP7iPGlS67xmHCT+ZPyHBCBklGp6oYAzg8=";
-  };
+    src = fetchFromGitHub {
+      owner = "BrowserWorks";
+      repo = "waterfox";
+      rev = "refs/tags/${version}";
+      hash = "sha256-MTlA/6R7opP7iPGlS67xmHCT+ZPyHBCBklGp6oYAzg8=";
+    };
 
-  requireSigning = false;
+    requireSigning = false;
 
-  allowAddonSideload = true;
+    allowAddonSideload = true;
 
-  branding = "waterfox/browser/branding";
+    branding = "waterfox/browser/branding";
 
-  extraConfigureFlags = [
+    extraConfigureFlags = [
       "--with-app-name=${pname}"
       "--with-app-basename=${applicationName}"
       "--with-unsigned-addon-scopes=app,system"
@@ -37,7 +38,16 @@ in
     ];
 
     meta = {
-      # ...
+      mainProgram = "waterfox";
+      description = "A privacy-focused, performance-oriented browser based on Firefox";
+      homepage = "https://www.waterfox.net/";
+      platforms = lib.platforms.unix;
+      badPlatforms = lib.platforms.darwin;
+      broken = stdenv.buildPlatform.is32bit;
+      # since Firefox 60, build on 32-bit platforms fails with "out of memory".
+      # not in `badPlatforms` because cross-compilation on 64-bit machine might work.
+      maxSilent = 14400; # 4h, double the default of 7200s (c.f. #129212, #129115)
+      license = lib.licenses.mpl20;
     };
   }).override {
     crashreporterSupport = false;
