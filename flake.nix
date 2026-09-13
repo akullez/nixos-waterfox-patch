b91699edbca77fc4b7cecbeca26cec7b626a1a0a
Alexey Kotovski <alexeykotovski80@gmail.com>
2026-09-13 20:11:27 +0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -10,13 +10,14 @@
     eachSystem = nixpkgs-stable.lib.genAttrs (import systems);
   in {
     packages = eachSystem (system:
-    let
-      pkgs = import nixpkgs-stable {
-        system = "${system}";
-      };
-    in {
-      default = self.packages.${system}.waterfox;
-      waterfox = pkgs.callPackage ./pkgs/waterfox/default.nix {};
-    });
+  let
+    # Меняем nixpkgs-stable на nixpkgs (это твоя unstable ветка)
+    pkgs = import nixpkgs {
+      system = "${system}";
+    };
+  in {
+    default = self.packages.${system}.waterfox;
+    waterfox = pkgs.callPackage ./pkgs/waterfox/default.nix {};
+  });
   };
 }
